54fbccbd9211c1f63f4d345aed5fb92a0134573f
josue <josuepalaci@gmail.com>
2020-09-18 15:07:02 -0600

    trabajando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { WeatherService } from "./services/weather.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit{
+    
+  weather;
+  
+  constructor(private weatherService: WeatherService){}
+
+  ngOnInit(): void {
+    // this.weatherService.getWeather('London', 'uk').subscribe(
+    //   datos => { this.weather = datos; console.log(datos);
+    //    },
+    //   error => { console.log(error); }
+    // )
+  }
+  
+
+  gettWeather(cityName: string, contryCode:string){
+    this.weatherService.getWeather(cityName, contryCode).subscribe(
+      datos => { this.weather = datos; console.log(datos);
+       },
+      error => { console.log(error); }
+    )
+  }
+
+  submitLocation(cityName: HTMLInputElement, contryCode: HTMLInputElement){
+
+    if(cityName.value && contryCode.value){
+
+      this.gettWeather(cityName.value, contryCode.value);
+      cityName.value = '';
+      contryCode.value = '';
+    } else {
+      alert('Inserte una ciudad');
+    }
+
+
+    cityName.focus();
+    return false;
+  }
+
+
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  uri: string = '';
+  apiKey = 'fc258d9731dae506d3b88f3f799f5e5f'
+
+  constructor( private htttCliente: HttpClient) { 
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  }
+
+  getWeather(cityName: string, contryCode: string){
+     return this.htttCliente.get(`${this.uri}${cityName},${contryCode}`);
+  }
+
+}
